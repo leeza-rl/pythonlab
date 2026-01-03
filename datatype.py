36156ac5065b9fb17864ebf4c4b39515e18bdb5a
Leeza_rl <leezarupapara@gmail.com>
2026-01-03 11:22:59 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,28 @@
+x = 20
+print(x)
+y = 50.5
+print(y)
+z = 1j
+print(z)
+a = True
+print(a)
+b = ["one","two","three"]
+print(b)
+c = ("apple","banana","cherry")
+print(c)
+d = range(7)
+print(d)
+e = {"name":"leeza","age":18}
+print(e)
+f = {"apple","banana","cherry"}
+print(f)
+g = frozenset({"apple","banana","cherry"})
+print(g)
+h = b"hello"
+print(h)
+i = bytearray(10)
+print(i)
+j = memoryview(bytes (6))
+print(j)
+k = None
+print (k)
