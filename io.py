d5d444332f21cd230b6a64c52485cff1b16e103b
Leeza_rl <leezarupapara@gmail.com>
2026-01-31 10:32:40 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,11 @@
+# output unsing print()
+print("hello world")
+
+print("welcome to atmiya")
+
+#input using input
+name = input("Enter your name :")
+print("hello",name)
+
+name = input("Enter your name : ")
+print("hey",name)
