70aae70dfd51dd21302e20287f5652efe383e3bd
Leeza_rl <leezarupapara@gmail.com>
2026-02-07 10:51:57 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,22 @@
+# loop 1
+age = int(input("Enter your age : "))
+if age >= 18:
+    print("yes you are eligeble for vote")
+else:
+    print("sorry you are not eligible for vote")
+    
+# loop 2
+marks = int(input("Enter your marks : "))
+if marks >= 90:
+    print("you will get grade A")
+elif marks>=60:
+    print("you will get grade B")
+else:
+    print("better luck next time")
+    
+# loop 3
+a = int(input("Enter the value : "))
+if a % 2:
+    print("odd value")
+else:
+    print("Even value")
