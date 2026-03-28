9969c2b4a18369835b04eb6e6cfee0523b74feb0
Leeza_rl <leezarupapara@gmail.com>
2026-03-28 11:29:25 +0530

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,25 @@
+"""n=int(input("enter no of lines: "))
+for i in range(1,n+10):
+    print("*" *i)"""
+    
+"""for i in range(1,4):
+    print("*" *i)"""
+    
+"""n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()"""
+    
+"""n=int(input("rnter number of lines: "))
+
+i=1
+while i<=n:
+    print("*" * i)
+    i +=1"""
+    
+"""n=int(input("enter no of lines: "))
+i=n
+while i>=1:
+    print("*" * i)
+    i -=1"""
